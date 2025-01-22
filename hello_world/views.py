4d87de4d70d1a3dfef72670af88de5ea5b3e225a
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-22 15:53:28 +0000

    Created post html, debugged views localhost to work, (blog/post), .

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = 'blog/post.html'
