e642ebe59dcf31fb001f9176fca821ef3df0cb1b
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-26 15:15:09 +0000

    Edited the paginate, to display 4 posts per page

--- hello_world/views.py
+++ hello_world/views.py
@@ -9,7 +9,7 @@ from .forms import CommentForm
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = 'blog/index.html'
-    paginate_by = 6
+    paginate_by = 4 # editing to 4
 
 def post_detail(request, slug):
     """
