5846d726fa5197f66bc9e84e8f8a8599c9701294
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-28 00:11:34 +0000

    Lighthouse testing, more changes to be made

--- hello_world/models.py
+++ hello_world/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder', transformation={'quality': "auto:good"}, blank=True, null=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
